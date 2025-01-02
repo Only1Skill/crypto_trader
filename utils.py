31b1f778f388a6165ec935027f67919753064886
Leonid <leonidplatonov23@gmail.com>
2025-01-03 02:59:29 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,9 +2,6 @@ import matplotlib.pyplot as plt
 
 
 def plot_stock_prices(stock):
-    """
-    Отображает историю изменения цен криптовалюты.
-    """
     plt.plot(stock.price_history, label=stock.name)
     plt.title(f"История цен: {stock.name}")
     plt.xlabel("Время")
